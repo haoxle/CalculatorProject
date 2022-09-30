e10f40c9e08e4582e1f375a8a9f92f4f3468251e
haoxle <hao-le@live.co.uk>
2022-09-30 21:41:33 +0100

    working calculator but a+b only

--- app.js
+++ app.js
@@ -61,42 +61,50 @@ for (var i = 0; i < numbers.length; i++) {
 const getOperator = (e) => {
   //storage as above
   const currNum = displayScreen.textContent;
-  const lastNum = currNum[currNum.length - 1];
+  const lastNum = currNum.substring(currNum.length - 3);
+  console.log(lastNum);
   if (
-    lastNum === "+" ||
-    lastNum === "-" ||
-    lastNum === "x" ||
-    lastNum === "÷"
+    lastNum === " + " ||
+    lastNum === " - " ||
+    lastNum === " x " ||
+    lastNum === " ÷ "
   ) {
     //adding substring returns all of the first sequence of numbers
     const newNum =
-      currNum.substring(0, currNum.length - 1) + e.target.textContent;
+      currNum.substring(0, currNum.length - 3) +
+      " " +
+      e.target.textContent +
+      " ";
     displayScreen.textContent = newNum;
   } else if (currNum.length == 0) {
     startCalc(e);
-  } else displayScreen.textContent += e.target.textContent;
+  } else displayScreen.textContent += ` ${e.target.textContent} `;
 };
-
 for (var i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", getOperator);
 }
 
-equals.addEventListener("click");
+const calcAns = (e) => {
+  const arrInString = displayScreen.textContent.split(" ");
+  console.log(arrInString);
+  const x = Number(arrInString[0]);
+  const y = Number(arrInString[2]);
+  switch (arrInString[1]) {
+    case "+":
+      displayScreen.textContent = x + y;
+      break;
+    case "-":
+      displayScreen.textContent = x - y;
+      break;
+    case "x":
+      displayScreen.textContent = x * y;
+      break;
+    case "÷":
+      displayScreen.textContent = x / y;
+      break;
+    default:
+      displayScreen.textContent = "0";
+  }
+};
 
-// const Calculatrice = (a, b, op) => {
-//   switch (op) {
-//     case "+":
-//       return a + b;
-//       break;
-//     case "-":
-//       return a - b;
-//       break;
-//     case "x":
-//       return a * b;
-//       break;
-//     case "÷":
-//       return a / b;
-//       break;
-//   }
-// }
-// console.log(Calculatrice(5, 5, "+"));
+equals.addEventListener("click", calcAns);
