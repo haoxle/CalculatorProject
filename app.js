a398936a93da4482f2b424a4a2241bde4ec70f75
haoxle <hao-le@live.co.uk>
2022-10-02 22:15:11 +0100

    pseudocodes

--- app.js
+++ app.js
@@ -4,34 +4,42 @@ const numbers = Array.from(document.querySelectorAll(".numBtn"));
 const operator = Array.from(document.querySelectorAll(".operator"));
 const equals = document.querySelector(".equalsBtn");
 
-let resultDisplayed = false;
 console.log(operator);
 console.log(numbers);
+
+//giving an initial value (will change as you use the calculator)
+let resultDisplayed = false;
 displayScreen.innerText = null;
 
+//When you click AC it will turn the calculator on and give you 0
 const startCalc = (e) => {
   e.preventDefault();
   displayScreen.textContent = 0;
 };
-
 acBtn.addEventListener("click", startCalc);
 
+//Enter numbers 0-9, can enter a lot of numbers
 const enteringNumbs = (e) => {
+  // Storing the currently displayed number and related unknown
   const currNum = displayScreen.textContent;
   const lastNum = currNum[currNum.length - 1];
 
+  //this gets rid of the 0 when you add numbers
   if (currNum[0] === "0") {
     displayScreen.textContent = "";
   }
 
+  //this allows you to add a sequence of numbers as we have initialised the output as false
   if (resultDisplayed === false) {
     displayScreen.textContent += e.target.textContent;
+    //as you have entered numbers if the last output is an operator it will set the resultdisplayed as false and allow you to add more numbers
   } else if (
     (resultDisplayed === true && lastNum == "+") ||
     lastNum == "-" ||
     lastNum == "x" ||
     lastNum == "÷"
   ) {
+    // this statement and the resultdisplayed on line 37 does not make a difference to the function, however for correctness it exists, when you press op  it will set result displayed to false
     resultDisplayed = false;
     displayScreen.textContent += e.target.textContent;
   } else startCalc(e);
@@ -41,9 +49,11 @@ for (var i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", enteringNumbs);
 }
 
+//This is similar to previous but allows to insert the operator iteself
 const getOperator = (e) => {
   const currNum = displayScreen.textContent;
   const lastNum = currNum.substring(currNum.length - 3);
+  //if the last number is an operator and you click on another operator, it will replace the previous operator with a new one.
   if (
     lastNum === " + " ||
     lastNum === " - " ||
@@ -56,14 +66,18 @@ const getOperator = (e) => {
       e.target.textContent +
       " ";
     displayScreen.textContent = newNum;
+    // 1 + = 0
   } else if (currNum.length == 0) {
     startCalc(e);
+
+    //if there is no operator there, it will add an operator to the end
   } else displayScreen.textContent += ` ${e.target.textContent} `;
 };
 for (var i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", getOperator);
 }
 
+//this is the equals function
 const calcAns = (e) => {
   const arrInString = displayScreen.textContent.split(" ");
   console.log(arrInString);
